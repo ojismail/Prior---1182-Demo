9de831815518430c4f84cabf5b43091abc53d1b0
Ozair Ismail <ojismail@gmail.com>
2023-03-22 09:11:10 +0000

    changed relationship between consultation and symptom. Also added history fieds to patient table

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -2,6 +2,6 @@ class Consultation < ApplicationRecord
   validates :status, inclusion: { in: %w(open active completed) }
 
   belongs_to :patient
-  has_many :symptom
+  belongs_to :symptom
   has_one :question_and_answer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_171012) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_090236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_171012) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "medical_history"
+    t.string "drug_history"
+    t.string "allergy_history"
   end
 
   create_table "question_and_answers", force: :cascade do |t|
