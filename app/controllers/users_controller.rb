ea631a0558fc834ec5d43fa4ddbfbce81472ec1b
Ozair Ismail <ojismail@gmail.com>
2023-03-24 11:55:01 +0000

    Sorted doctor/patient roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :phone)
+    params.require(:user).permit(:name, :email, :password, :role)
   end
 end
---
+++ db/migrate/20230324113638_change_role_type_in_users.rb
@@ -0,0 +1,5 @@
+class ChangeRoleTypeInUsers < ActiveRecord::Migration[6.1]
+  def change
+    change_column :users, :role, :integer, using: 'role::integer'
+  end
+end
