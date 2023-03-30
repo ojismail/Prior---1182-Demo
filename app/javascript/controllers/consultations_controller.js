f8bd33a2a75ca3efbf692b7af901c57df0b25646
Fabian Schaefer <fabianschaefer@hotmail.de>
2023-03-30 15:23:21 +0200

    Show collapses correctly

--- app/javascript/controllers/consultations_controller.js
+++ app/javascript/controllers/consultations_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    // add hidden class to all content elements
+    let contents = document.querySelectorAll("dd");
+    contents.forEach((content) => {
+      content.classList.add("hidden");
+      content.style.maxHeight = null;
+    });
   }
 
   next(event) {
     let nodes = document.getElementsByClassName(`${event.currentTarget.dataset.id}`)
     for (let el of nodes) {
       el.classList.toggle("hidden")
+      if (el.classList.contains("hidden")) {
+        el.nextElementSibling.style.maxHeight = null;
+      } else {
+        el.nextElementSibling.style.maxHeight = el.nextElementSibling.scrollHeight + "px";
+      }
     }
   }
 }
