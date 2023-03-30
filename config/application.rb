9dd31cf7e8183647d54bb0516d7d1781a6d549e7
Ozair Ismail <ojismail@gmail.com>
2023-03-30 09:35:09 +0100

    Reversed SSL changes

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Prior
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.action_controller.default_url_options = { host: 'https://www.prior.health' }
+    # config.action_controller.default_url_options = { host: 'https://www.prior.health' }
 
     # Configuration for the application, engines, and railties goes here.
     #
